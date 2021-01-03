67da9324a227ad23aed780be2e3d41af89783ce6
Nguyen Manh <manhnb1911@gmail.com>
2021-01-03 22:11:02 +0700

    fix footer

--- Origami/settings.py
+++ Origami/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Statics/'
 
 STATICFILES_DIRS = [
     STATIC_DIR,
