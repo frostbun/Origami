9b8e9e43dd43c24ab1517a87f19a3df6562560ae
Nguyen Manh <manhnb1911@gmail.com>
2021-01-04 20:10:37 +0700

    add user model

--- OrigamiApp/admin.py
+++ OrigamiApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from OrigamiApp.models import UserProfile
 
 # Register your models here.
+
+admin.site.register(UserProfile)
--- OrigamiApp/forms.py
+++ OrigamiApp/forms.py
@@ -1,1 +1,10 @@
 from django import forms
+from django.contrib.auth.models import User
+from OrigamiApp.models import UserProfile
+
+class UserSignUp(forms.ModelForm):
+    password = forms.CharField(widget = [forms.PasswordInput(), ])
+
+    class Meta():
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
