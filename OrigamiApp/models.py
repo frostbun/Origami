3a9940ee85dd5deb03600ac4cd8aece16fab9d5c
Nguyen Manh <manhnb1911@gmail.com>
2021-01-14 09:27:19 +0700

    fix comment

--- OrigamiApp/models.py
+++ OrigamiApp/models.py
@@ -23,3 +23,6 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cmts")
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + '\'s comment'
